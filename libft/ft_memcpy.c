c9a42ec6ead50fd7ab5705d12077ab30a26c0942
ghost <luisxl10@gmail.com>
2023-09-15 14:06:40 +0200

    falta proteger algunas funciones, pero pasa los test básicos

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -19,8 +19,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	aux_d = (unsigned char *) dst;
 	aux_s = (unsigned char *)src;
-	if (*aux_d == '\0' && *aux_s == '\0')
-		return (dst);
+	if (!aux_d && !aux_s)
+		return (NULL);
 	while (n--)
 		*aux_d++ = *aux_s++;
 	return (dst);
