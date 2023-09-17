731b09f27d4d13b785efd362c43611d3ce207654
ghost <luisxl10@gmail.com>
2023-09-18 01:45:19 +0200

    itoa correcto

--- libft/libft.h
+++ libft/libft.h
@@ -45,6 +45,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+char	*ft_itoa(int n);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
